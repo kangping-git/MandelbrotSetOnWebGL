d2caf728d213d708ae8aa00a29ed46e7659e20f0
kangping-git <69379510+kangping-git@users.noreply.github.com>
2023-01-08 08:49:08 +0900

    v1.0.2

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ window.addEventListener("load",() => {
         let frames = 0
         let lastFrameTime = new Date()
         let time = new Date()
-        let pointerX = 0
-        let pointerY = 0
+        let pointerX = -1
+        let pointerY = -1
         let clickX = 0
         let clickY = 0
         let nowX = 0
@@ -61,6 +61,31 @@ window.addEventListener("load",() => {
             gl.enableVertexAttribArray(positionAddress);
             gl.vertexAttribPointer(positionAddress, 3, gl.FLOAT, false, 0,0);
             gl.drawArrays(gl.TRIANGLES, 0, 6);
+            ctx.clearRect(0,0, MainCanvas.width, MainCanvas.height);
+            ctx.drawImage(canvas, 0, 0);
+            if (shiftKey){
+                ctx.fillStyle = "yellow"
+                ctx.strokeStyle = "yellow"
+                ctx.beginPath();
+                ctx.arc(_X,_Y,5,0,Math.PI * 2)
+                ctx.fill()
+                let zr = 0
+                let zi = 0
+                let cr = (_X-w/2.0)/zoom/(h/3.0)+nowX
+                let ci = (h/2.0-_Y)/zoom/(h/3.0)+nowY
+                for (let i = 0;i < _iter;++i){
+                    ctx.beginPath();
+                    ctx.moveTo((zr - nowX) * zoom * (h/3)+w/2.0,(nowY-zi) * zoom * (h/3)+h/2.0)
+                    let temp = zr
+                    zr = zr ** 2 - zi ** 2 + cr
+                    zi = 2 * temp * zi + ci
+                    ctx.lineTo((zr - nowX) * zoom * (h/3)+w/2.0,(nowY-zi) * zoom * (h/3)+h/2.0)
+                    ctx.stroke()
+                    ctx.beginPath();
+                    ctx.arc((zr - nowX) * zoom * (h/3)+w/2.0,(nowY-zi) * zoom * (h/3)+h/2.0,5,0,Math.PI * 2)
+                    ctx.fill()
+                }
+            }
             frames += 1
             if (new Date() - lastFrameTime >= 1000){
                 console.log(frames);
@@ -70,13 +95,21 @@ window.addEventListener("load",() => {
             requestAnimationFrame(render)
             time -= s
         }
+        window.addEventListener("contextmenu",(e) => {
+            e.preventDefault()
+        })
         window.addEventListener("mousedown",(e) => {
-            clickX = e.clientX
-            clickY = e.clientY
-            pointerX = nowX
-            pointerY = nowY
-            clicking = true
-            s = 0
+            if (e.button == 0){
+                clickX = e.clientX
+                clickY = e.clientY
+                pointerX = nowX
+                pointerY = nowY
+                clicking = true
+                s = 0
+            }else if (e.button == 2){
+                nowX = (_X-w/2.0)/zoom/(h/3.0)+nowX
+                nowY = (h/2.0-_Y)/zoom/(h/3.0)+nowY
+            }
             console.log(nowX,nowY);
         })
         window.addEventListener("mouseup",(e) => {
@@ -87,6 +120,8 @@ window.addEventListener("load",() => {
                 nowX = (clickX-e.clientX)/zoom/(h/3.0) + pointerX
                 nowY = (e.clientY-clickY)/zoom/(h/3.0) + pointerY
             }
+            _X = e.clientX
+            _Y = e.clientY
         })
         window.addEventListener("wheel",(e) => {
             if(e.deltaY > 0){
@@ -94,14 +129,21 @@ window.addEventListener("load",() => {
             }else{
                 zoom *= 1.1
             }
-            console.log(zoom);
+            if (shiftKey){
+                e.preventDefault()
+            }
         })
         window.addEventListener("resize",() => {
             w = window.innerWidth
             h = window.innerHeight
             canvas.width = w
             canvas.height = h
+            MainCanvas.width = w
+            MainCanvas.height = h
         })
+        let shiftKey = false
+        let _X = 0
+        let _Y = 0
         window.addEventListener("keydown",(e) => {
             if (e.key == "a"){
                 _iter += 1
@@ -109,19 +151,37 @@ window.addEventListener("load",() => {
             if (e.key == "d"){
                 _iter -= 1
             }
+            if (e.key == "A"){
+                _iter += 100
+            }
+            if (e.key == "D"){
+                _iter -= 100
+            }
             if (_iter <= 0){
                 _iter = 1
             }
+            if (e.key == "Control"){
+                shiftKey = true
+            }
             document.getElementById("Iteration").innerText = "Iteration:" + _iter
+        })
+        window.addEventListener("keyup",(e) => {
+            if (e.key == "Control"){
+                shiftKey = false
+            }
         })
         render()
     }
     const canvas = document.getElementById("glcanvas")
+    const MainCanvas = document.getElementById("MainCanvas")
     let w = window.innerWidth
     let h = window.innerHeight
     canvas.width = w
     canvas.height = h
+    MainCanvas.width = w
+    MainCanvas.height = h
     var gl = canvas.getContext("webgl")
+    var ctx = MainCanvas.getContext("2d")
     let element = document.createElement("div")
     element.id = "FPS"
     document.body.appendChild(element)
@@ -140,7 +200,7 @@ window.addEventListener("load",() => {
     element.id = "Iteration"
     document.body.appendChild(element)
     document.getElementById("Iteration").style.position = "fixed"
-    document.getElementById("Iteration").style.top = "25px"
+    document.getElementById("Iteration").style.top = "29px"
     document.getElementById("Iteration").style.left = "0px"
     document.getElementById("Iteration").style.backgroundColor = "rgba(0,0,0,0.7)"
     document.getElementById("Iteration").style.color = "greenyellow"
